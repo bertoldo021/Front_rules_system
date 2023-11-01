f6049a302ca7e7e34182db9a40f2c5fbe19005a6
bertoldo rohden <bertoldorohden@gmail.com>
2023-10-31 23:57:49 -0300

    On branch front_initial2 Changes to be committed: new file: 	src/app/about/about.component.css new file: 	src/app/about/about.component.html new file: 	src/app/about/about.component.ts modified: 	src/app/about/about.module.ts modified: 	src/app/home/home.component.html modified: 	src/app/home/home.module.ts modified: 	src/app/recommendation/recommendation.component.html modified: 	src/index.html

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  // ... outras rotas, se necessário
+  
 ];
 
 @NgModule({
